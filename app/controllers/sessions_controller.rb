10b6579d5180c9a6f79874823c0e221006f5663e
Huni-Tuch <hunibert.tuch@kellogg.northwestern.edu>
2023-03-03 05:53:05 +0000

    gotta work on this

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+      # authenticate the user
+      @user = User.find_by({ "email" => params["email"] })
+      if @user
+        if BCrypt::Password.new(@user["password"]) == params["password"]
+          session["user_id"] = @user["id"]
+          redirect_to "/places"
+        else
+          flash["notice"] = "Please enter username and password"
+          redirect_to "/login"
+        end
+      else
+        flash["notice"] = "Please enter username and password"
+        redirect_to "/login"
+      end
   end
 
   def destroy
+    flash["notice"] = "#TacosTacosTacos."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
